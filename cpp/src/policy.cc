a27d8d075843f1643057a76d50844872f1c16448
hmx <3523763591@qq.com>
2024-04-07 20:30:37 +0800

    add another select IO task function, get judgement fault

--- cpp/src/policy.cc
+++ cpp/src/policy.cc
@@ -9,6 +9,10 @@ using namespace std;
 map<int, Event::Task> TaskQueue;
 map<int, Event::Task> TaskIO;
 
+// kHigh,kLow各一个map
+vector<map<int, Event::Task>> priorityTaskQueue(2);
+vector<map<int, Event::Task>> priorityTaskIO(2);
+
 int cur_time = -1;
 
 // 从CPU任务队列移除任务
@@ -37,7 +41,7 @@ void removeTaskFromIOQueue(map<int, Event::Task> *IOQueue, int taskId)
   }
 }
 
-int selectNextIOTask(int current_io)
+int selectNextIOTask1(int current_io)
 {
 
   if (current_io == 0)
@@ -57,6 +61,53 @@ int selectNextIOTask(int current_io)
       }
       if (!flag) // 全超过截止时间
         tmp = TaskIO.begin()->second;
+      // tmp.taskId = 0;
+
+      return tmp.taskId;
+    }
+  }
+  return current_io;
+}
+
+int selectNextIOTask2(int current_io)
+{
+
+  if (current_io == 0)
+  {
+    if (!priorityTaskIO[0].empty())
+    {
+      int flag = 0;
+      Event::Task tmp;
+      for (auto iter = priorityTaskIO[0].begin(); iter != priorityTaskIO[0].end(); iter++)
+      {
+        if (iter->first > cur_time) //&& iter->second.priority == Event::Task::Priority::kHigh
+        {
+          tmp = iter->second;
+          flag = 1;
+          break;
+        }
+      }
+      if (!flag) // 全超过截止时间
+        tmp = priorityTaskIO[0].begin()->second;
+      // tmp.taskId = 0;
+
+      return tmp.taskId;
+    }
+    else
+    {
+      int flag = 0;
+      Event::Task tmp;
+      for (auto iter = priorityTaskIO[1].begin(); iter != priorityTaskIO[1].end(); iter++)
+      {
+        if (iter->first > cur_time) //&& iter->second.priority == Event::Task::Priority::kHigh
+        {
+          tmp = iter->second;
+          flag = 1;
+          break;
+        }
+      }
+      if (!flag) // 全超过截止时间
+        tmp = priorityTaskIO[1].begin()->second;
       // tmp.taskId = 0;
 
       return tmp.taskId;
@@ -85,15 +136,17 @@ int selectNextCPUTask()
 
   return tmp.taskId;
 }
+
 Action policy(const std::vector<Event> &events, int current_cpu,
               int current_io)
 {
-
   Action choose;
   choose.cpuTask = current_cpu;
   choose.ioTask = current_io;
   for (const auto &event : events)
   {
+    int priority = (event.task.priority == Event::Task::Priority::kHigh) ? 0 : 1;
+    int tId = event.task.taskId;
     switch (event.type)
     {
     case Event::Type::kTimer:
@@ -101,42 +154,27 @@ Action policy(const std::vector<Event> &events, int current_cpu,
       break;
 
     case Event::Type::kTaskArrival:
-      TaskQueue.insert(map<int, Event::Task>::value_type(event.task.deadline, event.task));
+      TaskQueue[event.task.deadline] = event.task;
+      priorityTaskQueue[priority][event.task.deadline] = event.task;
       break;
 
     case Event::Type::kTaskFinish:
-      for (auto iter = TaskQueue.begin(); iter != TaskQueue.end(); iter++)
-      {
-        if (iter->second.taskId == event.task.taskId)
-        {
-          TaskQueue.erase(iter);
-          break;
-        }
-      }
+      removeTaskFromCPUQueue(&TaskQueue, tId);
+      removeTaskFromCPUQueue(&priorityTaskQueue[priority], tId);
       break;
 
     case Event::Type::kIoRequest:
-      TaskIO.insert(map<int, Event::Task>::value_type(event.task.deadline, event.task));
-      for (auto iter = TaskQueue.begin(); iter != TaskQueue.end(); iter++)
-      {
-        if (iter->second.taskId == event.task.taskId)
-        {
-          TaskQueue.erase(iter);
-          break;
-        }
-      }
+      TaskIO[event.task.deadline] = event.task;
+      priorityTaskIO[priority][event.task.deadline] = event.task;
+      removeTaskFromCPUQueue(&TaskQueue, tId);
+      removeTaskFromCPUQueue(&priorityTaskQueue[priority], tId);
       break;
 
     case Event::Type::kIoEnd:
-      TaskQueue.insert(map<int, Event::Task>::value_type(event.task.deadline, event.task));
-      for (auto iter = TaskIO.begin(); iter != TaskIO.end(); iter++)
-      {
-        if (iter->second.taskId == event.task.taskId)
-        {
-          TaskIO.erase(iter);
-          break;
-        }
-      }
+      TaskQueue[event.task.deadline] = event.task;
+      priorityTaskQueue[priority][event.task.deadline] = event.task;
+      removeTaskFromIOQueue(&TaskIO, tId);
+      removeTaskFromIOQueue(&priorityTaskIO[priority], tId);
       break;
 
     default:
@@ -145,44 +183,10 @@ Action policy(const std::vector<Event> &events, int current_cpu,
   }
 
   // 选择io任务
-  if (current_io == 0)
-  {
-    if (!TaskIO.empty())
-    {
-      int flag = 0;
-      Event::Task tmp;
-      for (auto iter = TaskIO.begin(); iter != TaskIO.end(); iter++)
-      {
-        if (iter->first > cur_time)
-        {
-          tmp = iter->second;
-          flag = 1;
-          break;
-        }
-      }
-      if (!flag) // 全超过截止时间
-        tmp = TaskIO.begin()->second;
+  choose.ioTask = selectNextIOTask2(current_io);
 
-      choose.ioTask = tmp.taskId;
-    }
-  }
   // 选择cpu任务
-  int flag = 0;
-  Event::Task tmp;
-  for (auto iter = TaskQueue.begin(); iter != TaskQueue.end(); iter++)
-  {
-    if (iter->first > cur_time)
-    {
-      flag = 1;
-      tmp = iter->second;
-      break;
-    }
-  }
-
-  if (!flag)
-    tmp = TaskQueue.begin()->second;
-
-  choose.cpuTask = tmp.taskId;
+  choose.cpuTask = selectNextCPUTask();
 
   return choose;
 }
